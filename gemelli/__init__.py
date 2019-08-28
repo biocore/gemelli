38be609822aa2ee760f42fa78be099d09d95324b
cameronmartino <cameronmartino@gmail.com>
2019-08-28 16:20:10 -0700

    ver bump

--- gemelli/__init__.py
+++ gemelli/__init__.py
@@ -6,4 +6,4 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
