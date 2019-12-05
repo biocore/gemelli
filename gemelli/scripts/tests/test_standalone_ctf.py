2cd051c35501604624b332819dddde871b5723bd
cameronmartino <cameronmartino@gmail.com>
2019-12-05 12:54:11 -0800

    assertLessThan -> assertAlmostEqual

--- gemelli/scripts/tests/test_standalone_ctf.py
+++ gemelli/scripts/tests/test_standalone_ctf.py
@@ -57,9 +57,9 @@ class Test_standalone_rpca(unittest.TestCase):
         # Check that the distance matrix matches our expectations
         comp_col = ['PC1', 'PC2', 'PC3']
         cent_ = samp_res[comp_col].mean().values.max()
-        self.assertLessEqual(cent_, 1e-10)
+        self.assertAlmostEqual(cent_, 0)
         cent_ = feat_res[comp_col].mean().values.max()
-        self.assertLessEqual(cent_, 1e-10)
+        self.assertAlmostEqual(cent_, 0)
         # check matched
         assert_allclose(absolute_sort(samp_res[comp_col].values),
                         absolute_sort(samp_exp[comp_col].values),
