74149fb2e8d8eb1e94f7a2ce2b8bc209f93debe2
cameronmartino <cameronmartino@gmail.com>
2021-01-03 17:58:11 -0800

    add phylogenetic transformations

--- gemelli/preprocessing.py
+++ gemelli/preprocessing.py
@@ -9,10 +9,14 @@
 import warnings
 import numpy as np
 from biom import Table
+from skbio import TreeNode
 from .base import _BaseConstruct
+from gemelli._defaults import DEFAULT_MTD
+from skbio.diversity._phylogenetic import _nodes_by_counts
+from skbio.diversity._util import _vectorize_counts_and_tree
 
 
-def tensor_rclr(T):
+def tensor_rclr(T, branch_lengths=None):
     """
     Robust clr transform. is the approximate geometric mean of X.
 
@@ -81,7 +85,8 @@ def tensor_rclr(T):
 
     if len(T.shape) < 3:
         # tensor_rclr on 2D matrix
-        M_tensor_rclr = matrix_rclr(T.transpose().copy()).T
+        M_tensor_rclr = matrix_rclr(T.transpose().copy(),
+                                    branch_lengths=branch_lengths).T
         M_tensor_rclr[~np.isfinite(M_tensor_rclr)] = 0.0
         return M_tensor_rclr
     else:
@@ -97,13 +102,13 @@ def tensor_rclr(T):
         M = T.reshape(np.product(T.shape[:len(T.shape) - 1]),
                       T.shape[-1])
         with np.errstate(divide='ignore', invalid='ignore'):
-            M_tensor_rclr = matrix_rclr(M)
+            M_tensor_rclr = matrix_rclr(M, branch_lengths=branch_lengths)
         M_tensor_rclr[~np.isfinite(M_tensor_rclr)] = 0.0
         # reshape to former tensor and return tensors
         return M_tensor_rclr.reshape(T.shape).transpose(reverse_T)
 
 
-def matrix_rclr(M):
+def matrix_rclr(mat, branch_lengths=None):
     """
     Robust clr transform helper function.
     This function is built for mode 2 tensors,
@@ -135,36 +140,37 @@ def matrix_rclr(M):
 
     """
     # ensure array is at least 2D
-    M = np.atleast_2d(np.array(M))
+    mat = np.atleast_2d(np.array(mat))
     # ensure array not more than 2D
-    if M.ndim > 2:
+    if mat.ndim > 2:
         raise ValueError("Input matrix can only have two dimensions or less")
     # ensure no neg values
-    if (M < 0).any():
+    if (mat < 0).any():
         raise ValueError('Array Contains Negative Values')
     # ensure no undefined values
-    if np.count_nonzero(np.isinf(M)) != 0:
+    if np.count_nonzero(np.isinf(mat)) != 0:
         raise ValueError('Data-matrix contains either np.inf or -np.inf')
     # ensure no missing values
-    if np.count_nonzero(np.isnan(M)) != 0:
+    if np.count_nonzero(np.isnan(mat)) != 0:
         raise ValueError('Data-matrix contains nans')
-    # closure following procedure in
-    # skbio.stats.composition.closure
-    M_log = M / M.sum(axis=1, keepdims=True)
-    # log transform before geo-mean
-    M_log = np.log(M_log.squeeze())
-    mask = [True] * np.product(M_log.shape)
-    mask = np.array(mask).reshape(M_log.shape)
-    mask[np.isfinite(M_log)] = False
+    # take the log of the sample centered data
+    if branch_lengths is not None:
+        mat = np.log(matrix_closure(matrix_closure(mat) * branch_lengths))
+    else:
+        mat = np.log(matrix_closure(mat))
+    # generate a mask of missing values
+    mask = [True] * mat.shape[0] * mat.shape[1]
+    mask = np.array(mat).reshape(mat.shape)
+    mask[np.isfinite(mat)] = False
     # sum of rows (features)
-    M_tensor_rclr = np.ma.array(M_log, mask=mask)
-    # approx. geometric mean of the features
-    gm = M_tensor_rclr.mean(axis=-1, keepdims=True)
-    # subtracted to center log
-    M_tensor_rclr = (M_tensor_rclr - gm).squeeze().data
-    # ensure any missing are zero again
-    M_tensor_rclr[~np.isfinite(M_log)] = np.nan
-    return M_tensor_rclr
+    lmat = np.ma.array(mat, mask=mask)
+    # perfrom geometric mean
+    gm = lmat.mean(axis=-1, keepdims=True)
+    # center with the geometric mean
+    lmat = (lmat - gm).squeeze().data
+    # mask the missing with nan
+    lmat[~np.isfinite(mat)] = np.nan
+    return lmat
 
 
 def rclr_transformation(table: Table) -> Table:
@@ -177,6 +183,248 @@ def rclr_transformation(table: Table) -> Table:
                   table.ids('observation'),
                   table.ids('sample'))
     return table
+
+
+def phylogenetic_rclr_transformation(table: Table,
+                                     phylogeny: TreeNode,
+                                     min_depth: int = DEFAULT_MTD,
+                                     min_splits: int = DEFAULT_MTD,
+                                     max_postlevel: int = DEFAULT_MTD) -> (
+                                         Table, Table, TreeNode):
+    """
+    Takes biom table and returns fast_unifrac style
+    vectorized count table and a matrix_rclr
+    transformed biom table.
+
+    """
+    # build the vectorized table
+    counts_by_node, tree_index, branch_lengths, fids, otu_ids\
+        = fast_unifrac(table, phylogeny, min_depth, min_splits, max_postlevel)
+    # Robust-clt (matrix_rclr) preprocessing
+    rclr_table = matrix_rclr(counts_by_node, branch_lengths=branch_lengths)
+    # import transformed matrix into biom.Table
+    rclr_table = Table(rclr_table,
+                       table.ids('observation'),
+                       table.ids('sample'))
+    # import expanded matrix into biom.Table
+    counts_by_node = biom.Table(counts_by_node.T,
+                                fids, table.ids())
+
+    return counts_by_node, rclr_table, phylogeny
+
+
+def matrix_closure(mat):
+    """
+    Simillar to the skbio.stats.composition.closure function.
+    Performs closure to ensure that all elements add up to 1.
+    However, this function allows for zero rows. This results
+    in rows that may contain missing (NaN) vlues. These
+    all zero rows may occur as a product of a tensor slice and
+    is dealt later with the tensor restructuring and factorization.
+
+    Parameters
+    ----------
+    mat : array_like
+       a matrix of proportions where
+       rows = compositions
+       columns = components
+    Returns
+    -------
+    array_like, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from gemelli.preprocessing import matrix_closure
+    >>> X = np.array([[2, 2, 6], [0, 0, 0]])
+    >>> closure(X)
+    array([[ 0.2,  0.2,  0.6],
+           [ nan,  nan,  nan]])
+     
+    """
+
+    mat = np.atleast_2d(mat)
+    mat = mat / mat.sum(axis=1, keepdims=True)
+    return mat.squeeze()
+
+
+def fast_unifrac(table, tree, min_depth=0, min_splits=0, max_postlevel=0):
+    """
+    A wrapper to return a vectorized Fast UniFrac
+    algorithm. The nodes up the tree are summed
+    and exposed as vectors in the matrix. The
+    closed matrix is then multipled by the
+    branch lengths to phylogenically
+    weight the data. 
+
+    Parameters
+    ----------
+    table : biom.Table
+       A biom table of counts.
+    tree: skbio.TreeNode
+       Tree containing the features in the table.
+    min_depth: int
+        Minimum number of total number of
+        descendants (tips) to include a node.
+        Default value of zero will retain all nodes
+        (including tips).
+    min_splits: int
+        Minimum number of total number of
+        splits to include a node.
+        Default value of zero will retain all nodes
+        (including tips).
+    max_postlevel: int
+        Minimum allowable max postlevel
+        splits to include a node.
+        Default value of zero will retain all nodes
+        (including tips).
+    Returns
+    -------
+    counts_by_node: array_like, np.float64
+       A matrix of counts with internal nodes
+       vectorized.
+    tree_index: dict
+        A housekeeping dictionary.
+    branch_lengths: array_like, np.float64
+        An array of branch lengths.
+    fids: list
+        A list of feature IDs matched to tree_index['id'].
+    otu_ids: list
+        A list of the original table OTU IDs (tips).
+    Examples
+    --------
+    TODO
+     
+    """
+
+    # original table
+    bt_array = table.matrix_data.toarray()
+    otu_ids = table.ids('observation')
+    # expand the vectorized table
+    counts_by_node, tree_index, branch_lengths \
+        = _vectorize_counts_and_tree(bt_array.T, otu_ids, tree)
+    # check branch lengths
+    if sum(branch_lengths) == 0:
+        raise ValueError('All tree branch lengths are zero. '
+                         'This will result in a table of zero features.')
+    # drop zero sum features (non-optional for CTF/RPCA)
+    keep_zero = counts_by_node.sum(0) > 0
+    # drop zero branch_lengths (no point to keep it)
+    node_branch_zero = branch_lengths.sum(0) > 0
+    # calculate node information
+    calc_split_metrics(tree)
+    # check to ensure tree filters make sense
+    if tree.root().n <= min_depth:
+        raise ValueError('min_depth is equal to tree root value, '
+                         'this will result in a table of zero features.')
+    if np.max(tree.root().postlevels) <= max_postlevel:
+        raise ValueError('max_postlevel is equal to max postlevel at tree'
+                         ' root, this will result in a table of zero features.')
+    if tree.root().splits <= min_splits:
+        raise ValueError('min_splits is equal to number of splits at tree'
+                         ' root, this will result in a table of zero features.')
+    # create index to filter table
+    keep_node_depth = np.array([True] * counts_by_node.shape[1])
+    keep_node_splits = np.array([True] * counts_by_node.shape[1])
+    keep_node_postlevel = np.array([True] * counts_by_node.shape[1])
+    for count_index_, node_ in tree_index['id_index'].items():
+        # total number nodes under is more than min_depth
+        keep_node_depth[count_index_] = node_.n > min_depth
+        # number of splits
+        keep_node_splits[count_index_] = node_.splits >= min_splits
+        # number of postlevel
+        keep_node_postlevel[count_index_] = np.max(node_.postlevels) > max_postlevel
+    # get joint set of nodes to keep
+    keep_node = (keep_node_depth & keep_node_postlevel &\
+                 keep_node_splits & keep_zero & node_branch_zero)
+    # check all filter
+    if sum(keep_node) == 0:
+        raise ValueError('Combined table and tree filters resulted'
+                         ' in a table of zero features.')        
+    # subset the table
+    counts_by_node = counts_by_node[:, keep_node]
+    branch_lengths = branch_lengths[keep_node]
+    fids = ['n' + i for i in list(tree_index['id'][keep_node].astype(str))]
+    tree_index['keep'] = {i:v for i, v in enumerate(keep_node)}
+    # re-label tree to return with labels
+    tree_relabel = {tid_:tree_index['id_index'][int(tid_[1:])]
+                    for tid_ in fids}
+    # re-name nodes to match vectorized table
+    for new_id, node_ in tree_relabel.items():
+        if node_.name in otu_ids:
+            # replace table name (leaf - nondup)
+            fids[fids.index(new_id)] = node_.name
+        else:
+            # replace tree name (internal)
+            node_.name = new_id
+
+    return counts_by_node, tree_index, branch_lengths, fids, otu_ids
+
+
+def calc_split_metrics(tree):
+    """Calculate topology split-related metrics.
+    Parameters
+    ----------
+    tree : skbio.TreeNode
+        tree to calculate metrics
+    Notes
+    -----
+    The following metrics will be calculated for each node:
+    - n : int
+        number of descendants (tips)
+    - splits : int
+        total number of splits from tips
+    - prelevel : int
+        number of nodes from root
+    Examples
+    --------
+    >>> # Example from Fig. 9a of Puigbo, et al., 2009, J Biol:
+    >>> newick = '((((A,B)n9,C)n8,(D,E)n7)n4,((F,G)n6,(H,I)n5)n3,(J,K)n2)n1;'
+    >>> tree = TreeNode.read([newick])
+    >>> print(tree.ascii_art())
+                                            /-A
+                                  /n9------|
+                        /n8------|          \\-B
+                       |         |
+              /n4------|          \\-C
+             |         |
+             |         |          /-D
+             |          \\n7------|
+             |                    \\-E
+             |
+             |                    /-F
+    -n1------|          /n6------|
+             |         |          \\-G
+             |-n3------|
+             |         |          /-H
+             |          \\n5------|
+             |                    \\-I
+             |
+             |          /-J
+              \\n2------|
+                        \\-K
+    >>> calc_split_metrics(tree)
+    >>> tree.find('n3').n
+    4
+    >>> tree.find('n4').splits
+    4
+    >>> tree.find('n8').postlevels
+    [3, 3, 2]
+
+    """
+    # calculate bottom-up metrics
+    for node in tree.postorder(include_self=True):
+        if node.is_tip():
+            node.n = 1
+            node.splits = 0
+            node.postlevels = [1]
+        else:
+            children = node.children
+            node.n = sum(x.n for x in children)
+            node.splits = sum(x.splits for x in children) + 1
+            node.postlevels = [y + 1 for x in node.children for y in
+                               x.postlevels]
 
 
 class build(_BaseConstruct):
